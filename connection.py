7fca325d99368ae738cc80d421ebd8c4b8bc92bf
Mayerlyn R <maye.yrs23@gmail.com>
2025-09-16 10:47:34 -0300

    indentacion

--- connection.py
+++ connection.py
@@ -357,8 +357,6 @@ def traer_documentos_firmados(id: int) -> dict:
     )
 
     resultado = {"documento": None, "certificado": None}
-
-    
     for doc in documentos:
         if 'certificate' in doc['name'].lower():
             resultado["certificado"] = doc['datas']
@@ -369,21 +367,21 @@ def traer_documentos_firmados(id: int) -> dict:
 
 
 def notificar_firma(payload: dict):
-     """
-     Envía una notificación HTTP con los datos de la firma.
+    """
+    Envía una notificación HTTP con los datos de la firma.
 
-     Args:
-         payload (dict): Datos a enviar en la notificación.
+    Args:
+        payload (dict): Datos a enviar en la notificación.
 
-     Returns:
-         str: Mensaje de éxito o lanza error.
-     """
-     import requests
+    Returns:
+        str: Mensaje de éxito o lanza error.
+    """
+    import requests
 
-     headers = {'Content-Type': 'application/json'}
-     response = requests.post(url_notificaciones, headers=headers, data=json.dumps(payload))
-     response.raise_for_status()
-     return "Notificación enviada exitosamente"
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url_notificaciones, headers=headers, data=json.dumps(payload))
+    response.raise_for_status()
+    return "Notificación enviada exitosamente"
 
 
 def cancelar_documento_firma(doc_id: int):
